74f0c00d04d7188dd453e6fdda6443a5c4f7444e
pieeee <ahnafurp@gmail.com>
2023-03-19 21:11:04 +0600

    refactor subdomain

--- index.js
+++ index.js
@@ -5,35 +5,43 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (_, res) => {
+const subDomainWhiteList = ["ressler", "cooper", "reddington"];
+
+app.get("/", (req, res) => {
+  const subdomain = req.subdomains[0];
+
+  if (subDomainWhiteList.includes(subdomain)) {
+    res.send(`<h1>I am ${subdomain} 😎</h1>`);
+  }
+
   res.send(`
     <h1>App is running!! 🔥</h1>
     <ul>
         <li>
-        <a href='/ressler' target="_blank">Ressler</a>
+        <a href='https://ressler.suppaflash.com' target="_blank">Ressler</a>
         </li>
           <li>
-        <a href='/cooper' target="_blank">Cooper</a>
+        <a href='https://cooper.suppaflash.com' target="_blank">Cooper</a>
         </li>
           <li>
-        <a href='/reddington' target="_blank">Reddington</a>
+        <a href='https://reddington.suppaflash.com' target="_blank">Reddington</a>
         </li>
     </ul>
     `);
 });
 
-app.get("/:username", (req, res) => {
-  const availableUsernames = ["ressler", "cooper", "reddington"];
+// app.get("/:username", (req, res) => {
+//   const availableUsernames = ["ressler", "cooper", "reddington"];
 
-  if (!availableUsernames.includes(req.params.username)) {
-    res.status(404).end();
-  }
+//   if (!availableUsernames.includes(req.params.username)) {
+//     res.status(404).end();
+//   }
 
-  res.send(`
-    <a href='/'><-- Back</a>
-    <h1>I am ${req.params.username} 😎</h1>
-    `);
-});
+//   res.send(`
+//     <a href='/'><-- Back</a>
+//     <h1>I am ${req.params.username} 😎</h1>
+//     `);
+// });
 
 const PORT = 4000;
 app.listen(PORT, () => {
